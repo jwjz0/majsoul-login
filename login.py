6444d4e76fc1e49d26c62f68ab4fe1b10f452b1e
jwjz0 <jwjz0@users.noreply.github.com>
2026-06-18 10:53:15 +0800

    fix: 恢复窗口尺寸1280x800，坐标偏移是我改的锅

--- login.py
+++ login.py
@@ -84,7 +84,7 @@ for i in range(acccounts):
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=1280,800")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
 
